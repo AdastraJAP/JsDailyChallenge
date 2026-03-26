24e103e1ef33a1d6358eee28d29dce8f3eed8c3e
Piard Jose Alan <160079906+AdastraJAP@users.noreply.github.com>
2026-03-26 23:54:35 +0800

    day26

---
+++ flatten.js
@@ -0,0 +1,19 @@
+
+
+
+function flatten(array) {
+  const result = [];
+
+  for (const item of array) {
+    if (Array.isArray(item)) {
+      result.push(...flatten(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+
+console.log(flatten([1, 2, [3, [4, 5]], 6, 7]));
